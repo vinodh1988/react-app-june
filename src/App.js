59e2f60c1ba3fe911b4d85e4f4feb977e702012b
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-08 11:15:55 +0530

    new-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Box from './Box';
 
 const App = ()=>{
   return (
     <div>
         <h1>First React App</h1>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
     </div>
   )
 }
---
+++ src/Box.js
@@ -0,0 +1,10 @@
+
+const Box= ()=>{
+    return(
+        <div className="box">
+
+        </div>
+    )
+}
+
+export default Box;
